1ccea3ff6f90d26e8253d6795b84d8f5c195f860
W9359412 <79636802+W9359412@users.noreply.github.com>
2021-09-29 18:06:01 +0100

    Created 1st set of the game

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -2,11 +2,45 @@
 
 namespace GuessingGame
 {
+    class Game
+    {
+        static int Guess = 0;
+        static int Target = 5;
+        static string Input = "";
+        static Random RandomNumber = new Random();
+
+        public static void Play()
+        {
+            Target = RandomNumber.Next(10) + 1;
+            Console.Write("Guess what number I am thinking of... ");
+            Input = Console.ReadLine();
+            if (int.TryParse(Input, out Guess))
+            {
+                if (Guess == Target)
+                {
+                    Console.WriteLine("Congratulations! You guessed " + Guess + " and the number I was thinking of was " + Target + ".");
+                }
+                else
+                {
+                    Console.WriteLine("Too bad. You guessed " + Guess + " and the number I was thinking of was " + Target + ". Try again!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number.");
+                Console.WriteLine("Press enter to continue...");
+                Console.ReadLine();
+                Play();
+            }
+
+            Console.ReadKey();
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Game.Play();
         }
     }
 }
